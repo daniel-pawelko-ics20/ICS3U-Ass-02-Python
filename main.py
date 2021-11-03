0610fabf45c8c29868059ac74ffb2c175c92552e
Ubuntu <ubuntu@ip-172-31-85-153.ec2.internal>
2021-11-03 03:38:23 +0000

    finished

---
+++ main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# Created by: Daniel Pawelko
+# Created on: Oct 2021
+# This program will calculate area of decagon
+from math import sqrt
+
+
+def main():
+    # main function
+    print("We will be calculating the area of a decagon.")
+    # input
+    length = int(input("Enter the length of one side (mm): "))
+
+    # process
+    area = round((5 / 2) * pow(length, 2) * (sqrt(5 + 2 * sqrt(5))), 2)
+
+    # output
+    print(f"Area is {area} mm2")
+
+    # output finished
+    print("")
+    print("Done.")
+
+
+if __name__ == "__main__":
+    main()
